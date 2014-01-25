a040dcfad00e169ee0869d8e2fe2e201dec5c844
raunakmaskay <raunak.maskay@sproutify.com>
2014-01-25 17:16:02 +0545

    jukebox

---
+++ Juke.rb
@@ -0,0 +1,31 @@
+class JukeBox
+  attr_accessor :playlist, :index
+
+  def initialize
+    @playlist = ["a", "b"]
+    @index = 0
+    # puts list
+  end
+
+  def current
+    # puts playlist
+    playlist[index]
+  end
+
+  def next
+    @index+=1
+    playlist[index]
+  end
+
+  def previous
+    @index-=1
+    playlist[index]
+  end
+
+  def add(song)
+
+    @playlist << song
+  end
+
+ end
+
